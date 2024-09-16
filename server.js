2365e3a2d434436323a8e41662bf268248e21224
vikash <vp3637637@gmail.com>
2024-09-16 18:57:28 +0530

    changed client url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin :['http://localhost:5173'],
+    origin :['https://techstufff.vercel.app'],
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
