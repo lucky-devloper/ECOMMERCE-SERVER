f321b224d43bcf2ab5b84e654b2e8f5b59df3239
vikash <vp3637637@gmail.com>
2024-09-20 17:14:04 +0530

    added local ui route to server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin :['https://techstufff.vercel.app'],
+    origin :['https://techstufff.vercel.app','http://localhost:5173'],
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
