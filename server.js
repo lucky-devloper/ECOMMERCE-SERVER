57363ec4826bac00a3c5d60627597a75391c77b6
lucky-devloper <pritamsharmassm701@gmail.com>
2024-09-21 10:26:15 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const uploadRoute= require('./routes/uploadRoute')
 const userRoute= require('./routes/userRoute')
 const productRoute = require('./routes/productRoute')
 const cartRoute = require('./routes/cartRoute')
-const paymentRoute= require('./routes/paymentRoute')
 const orderRoute= require('./routes/orderRoute')
 const cors = require('cors')
 const dotenv= require('dotenv')
@@ -15,7 +13,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin :['https://techstufff.vercel.app','http://localhost:5173'],
+    origin :['https://hostedui.com','http://localhost:5173'],
     methods:['GET','POST','PATCH','DELETE'],
     credentials:true
 }))
@@ -34,11 +32,9 @@ app.get('/',(req,res)=>{
 
 
 
-app.use('/api',uploadRoute)
 app.use('/api',userRoute)
 app.use('/api',productRoute)
 app.use('/api',cartRoute)
-app.use('/api',paymentRoute)
 app.use('/api',orderRoute)
 const port=process.env.PORT || 3000
 app.listen(port,()=>{
